ad6f9d3c8b2c731cca6a181612aa8a4e8537a88c
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-10-05 19:52:40 +0200

    05/10/2023_good

--- dwes/form_reservations_select.php
+++ dwes/form_reservations_select.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Managment System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="1f3e8.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="./img/1f3e8.png">
 </head>
 <body>
-    <?php include ('header.html'); ?>
+    <?php include ('header.php'); ?>
     
-    <form action="" method="get">
+    <form action="db_reservations_select.php" method="get">
         <label for="">Day of arrival</label>
         <input type="date" name="date_in">
         <label for="">Day of exit</label>
--- dwes/header.php
+++ dwes/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel Managment System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="1f3e8.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="./img/1f3e8.png">
 </head>
 <body>
     <nav class="header">
