f759a41941fc7fe216d6105768805979d1796d2d
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-10-05 19:46:20 +0200

    05/10/2023

---
+++ form_reservations_select.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Managment System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="1f3e8.png">
+</head>
+<body>
+    <?php include ('header.html'); ?>
+    
+    <form action="" method="get">
+        <label for="">Day of arrival</label>
+        <input type="date" name="date_in">
+        <label for="">Day of exit</label>
+        <input type="date" name="date_out">
+        <input type="text">
+    </form>
+</body>
+</html>
