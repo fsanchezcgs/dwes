03600766fcda4cd08d7cbbd5abfc2ab5206d0d34
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-10-10 21:20:10 +0200

    10-10-2023

--- dwes/db/db_connect.php
+++ dwes/db/db_connect.php
@@ -1,9 +1,12 @@
 <?php
-    
     $user = "root";
     $password = "";
     $server = "localhost";
     $database = "hotel";
 
     $con = mysqli_connect($server, $user, $password, $database);
+
+    if(!$con) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 ?>
--- dwes/db_reservations_select.php
+++ dwes/db_reservations_select.php
@@ -2,5 +2,18 @@
     $date_in = $_POST['date_in'];
     $date_out = $_POST['date_out'];
 
-    
+    include('./db/db_connect.php');
+
+    $sql = "SELECT ro.room_id
+    FROM rooms AS ro
+    WHERE ro.room_id NOT IN (
+        SELECT re.room_id
+        FROM reservations AS re
+        WHERE date_in < $date_out AND date_out > $date_in)";
+
+    $result = mysqli_query($con, $sql);
+
+    $rooms = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    print_r($rooms);
 ?>
