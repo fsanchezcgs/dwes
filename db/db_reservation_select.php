9a503ff63958df867a0cf25d27ded79ec94a4ff5
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-11-14 21:14:21 +0100

    14-11-2023 part2

--- db/db_reservation_select.php
+++ db/db_reservation_select.php
@@ -3,11 +3,8 @@
     if(isset($_POST['submit'])) {
         include('./db_connect.php');
 
-        $customer_id = $_POST['customer_id'];
-
         $sql = "SELECT *
-                FROM 046_reservations
-                WHERE customer_id = $customer_id";
+                FROM 046_reservations";
 
         $result = mysqli_query($conn, $sql);
 
@@ -18,13 +15,16 @@
     <section class="d-flex justify-content-center flex-wrap">
     <?php foreach($reservations as $reservation) {?>
         <div class="card m-3">
-            <h5 class="card-title"><?php echo $reservation['customer_first_name']?></h5>
-            <form action="/student046/dwes/form/form_customer_update_call.php" method="post">
+            <h5 class="card-title"><?php echo $reservation['reservation_number']?></h5>
+            <div class="card-body">
+                <p class="card-text"><?php echo 'Customer ID: '.$reservations['customer_id'].'<br>'.$reservations['room_type']; if($reservations['room_type'] == 3) { echo 'Category: Family<br>Price: 80,00€'; } if($reservations['room_type'] == 4) { echo 'Category: Deluxe<br>Price: 110,00€'; } ?></p>
+            </div>
+            <form action="/student046/dwes/form/form_reservation_update_call.php" method="post">
                 <input type="number" name="customer_id" value="<?php echo $customer['customer_id']; ?>" hidden>
                 <input type="number" name="room_id" value="<?php echo $customer['room_id']; ?>" hidden>
-                <!-- <input type="number" name="customer_last_name" value="<?php echo $customer['customer_last_name']; ?>" hidden>
+                <input type="number" name="customer_last_name" value="<?php echo $customer['customer_last_name']; ?>" hidden>
                 <input type="number" name="customer_phone_number" value="<?php echo $customer['customer_phone_number']; ?>" hidden>
-                <input type="number" name="customer_email" value="<?php echo $customer['customer_email']; ?>" hidden> -->
+                <input type="number" name="customer_email" value="<?php echo $customer['customer_email']; ?>" hidden>
                 <input type="submit" value="submit">
             </form>
         </div>
