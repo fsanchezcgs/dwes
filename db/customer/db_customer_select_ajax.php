5395ace514a9c096c3e05d8b8887b8ac1e1f1085
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-01-19 19:47:26 +0100

    19-01-2024

---
+++ db/customer/db_customer_select_ajax.php
@@ -0,0 +1,16 @@
+<?php
+    include ($_SERVER['DOCUMENT_ROOT'].'/student046/dwes/header.php');
+    include ($_SERVER['DOCUMENT_ROOT'].'/student046/dwes/db/function/selectFunctions.php');
+    include($_SERVER['DOCUMENT_ROOT'].'/student046/dwes/db/db_connect.php');
+    $q = $_GET["q"];
+
+    if($q != "") {
+    $sql = "SELECT *
+            FROM 046_customers";
+
+    $result = mysqli_query($conn, $sql);
+
+    $customers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    showCustomers($customers);
+    }
+?>
