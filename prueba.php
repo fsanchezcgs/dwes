2128c86e277338a5f3c1c85c1d171f0387f3ca1e
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-01-23 21:11:01 +0100

    23-01-2024

--- prueba.php
+++ prueba.php
@@ -6,27 +6,22 @@
     }
   </style>
 <script>
-function showHint(str) {
-  if (str.length == 0) {
-    document.getElementById("txtHint").innerHTML = "";
-    return;
-  } else {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("txtHint").innerHTML = this.responseText;
-      }
-    };
-    xmlhttp.open("GET", "/student046/dwes/db/customer/db_customer_select_ajax.php?q=" + str, true);
-    xmlhttp.send();
-  }
+function showHint() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  let jsonMao = <?php echo file_get_contents("http://dataservice.accuweather.com/currentconditions/v1/2-305482_1_AL?apikey=T2UV4TPGQGS5IGWEEKABCK45n9pyh1HQ") ?>;
+  console.log(jsonMao);
 }
 </script>
 
 <p><b>Start typing a name in the input field below:</b></p>
 <form action="">
   <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+  <input type="button" value="JSON" id="fname" name="fname" onclick="showHint()">
 </form>
 <p>Suggestions: <span id="txtHint"></span></p>
 <?php include ($_SERVER['DOCUMENT_ROOT'].'/student046/dwes/footer.php'); ?>
